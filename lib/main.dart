f11428e649f58e93b58a9f438a43445946f2b8d0
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-01-19 12:24:11 -0500

    wallet page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_getx/firebase_options.dart';
-import 'package:food_delivery_getx/pages/login_page.dart';
+import 'package:food_delivery_getx/pages/wallet_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: LoginPage(),
+      home: WalletPage(),
     );
   }
 }
--- lib/pages/wallet_page.dart
+++ lib/pages/wallet_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_getx/widget/widget_support.dart';
 
 class WalletPage extends StatefulWidget {
   const WalletPage({super.key});
@@ -8,8 +9,79 @@ class WalletPage extends StatefulWidget {
 }
 
 class _WalletPageState extends State<WalletPage> {
+  final List amounts = [100, 500, 1000, 2000];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 50),
+        child: Column(
+          children: [
+            Material(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Center(child: Text('Wallet', style: WidgetSupport.boldTextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold))),
+              ),
+            ),
+            SizedBox(height: 30),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(color: Color(0xFFF2F2F2)),
+              child: Row(
+                children: [
+                  Image.asset('images/wallet.png', height: 60),
+                  SizedBox(width: 40),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Your Wallet", style: WidgetSupport.boldTextStyle(color: Colors.black26, fontSize: 13, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 5),
+                      Text("\$100", style: WidgetSupport.boldTextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Add Money", style: WidgetSupport.boldTextStyle(color: Colors.black, fontWeight: FontWeight.w600)),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(amounts.length, (index) => _itemAmount(amounts[index])),
+                  ),
+                  SizedBox(height: 60),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF008080), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text('Add Money', style: WidgetSupport.boldTextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 17)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _itemAmount(double amount) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(border: Border.all(color: Color(0xFFE9E2E2)), borderRadius: BorderRadius.circular(5)),
+      child: Text("\$${amount.toString()}", style: WidgetSupport.boldTextStyle(color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 18)),
+    );
   }
 }
