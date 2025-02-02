3eac23859861d735b1f7ccb0f6da5a76397bc6e9
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-02-02 00:20:58 -0500

    add: admin page home

---
+++ lib/pages/add_food_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddFoodPage extends StatefulWidget {
+  const AddFoodPage({super.key});
+
+  @override
+  State<AddFoodPage> createState() => _AddFoodPageState();
+}
+
+class _AddFoodPageState extends State<AddFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/home_admin_page.dart
+++ lib/pages/home_admin_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_getx/pages/add_food_page.dart';
+import 'package:food_delivery_getx/widget/widget_support.dart';
 
 class HomeAdminPage extends StatefulWidget {
   const HomeAdminPage({super.key});
@@ -12,10 +14,36 @@ class _HomeAdminPageState extends State<HomeAdminPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+        margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 20.0),
         child: Column(
           children: [
-            Center(child: Text('Home Admin')),
+            Center(child: Text("Home Admin", style: WidgetSupport.boldTextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 25))),
+            SizedBox(height: 50),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => AddFoodPage()));
+              },
+              child: Material(
+                elevation: 10,
+                borderRadius: BorderRadius.circular(10),
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10)),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(6),
+                          child: Image.asset("images/food.jpg", height: 100, width: 100, fit: BoxFit.cover),
+                        ),
+                        SizedBox(width: 30),
+                        Text("Add Food Items", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
